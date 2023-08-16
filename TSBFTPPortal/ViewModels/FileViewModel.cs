72d9ecbd74502322c4fc37c041ed4e6798e55e16
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2023-08-16 11:31:16 -0600

    Created View models

---
+++ TSBFTPPortal/ViewModels/FileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSBFTPPortal.ViewModels
+{
+	public class FileViewModel : ViewModelBase
+	{
+		public string? Name { get; set; }
+		public string? Path { get; set; }
+		public bool IsDirectory => false;
+		public bool IsFile => true;
+	}
+}
