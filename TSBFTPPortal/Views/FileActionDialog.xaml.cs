627724aa5e1035424318d6c32d0ff6b1a5afa175
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2023-09-13 10:36:53 -0600

    Styled dialog box

--- TSBFTPPortal/Views/FileActionDialog.xaml.cs
+++ TSBFTPPortal/Views/FileActionDialog.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Windows;
+﻿using System.Windows;
 
 namespace TSBFTPPortal.Views
 {
@@ -11,13 +10,6 @@ namespace TSBFTPPortal.Views
 		public FileActionDialog()
 		{
 			InitializeComponent();
-			Loaded += FileActionDialog_Loaded; // Attach the Loaded event handler
-		}
-
-		private void FileActionDialog_Loaded(object sender, RoutedEventArgs e)
-		{
-			// DataContext is now initialized; you can access it here.
-			Debug.WriteLine("DataContext of FileActionDialog: " + DataContext.GetType().Name);
 		}
 	}
 }
