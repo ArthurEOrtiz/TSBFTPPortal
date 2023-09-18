14b0aa9aa08356e67fcf27e3b926f24f31a8f83c
Arthur Ortiz <arortiz@tax.idaho.gov>
2023-09-18 11:20:48 -0600

    Refactored all other view models to use more ViewModel Base

--- TSBFTPPortal/ViewModels/AdminAppTimberTreeViewViewModel.cs
+++ TSBFTPPortal/ViewModels/AdminAppTimberTreeViewViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using Serilog;
 using System.Collections.ObjectModel;
-using System.IO;
 using TSBFTPPortal.Models;
 using TSBFTPPortal.Services;
 
@@ -9,46 +8,16 @@ namespace TSBFTPPortal.ViewModels
 	public class AdminAppTimberTreeViewViewModel : ViewModelBase
 	{
 		public County SelectedCounty { get; }
-		public readonly FtpService _ftpService;
 		public SearchBarViewModel SearchBarViewModel { get; }
 
 		public AdminAppTimberTreeViewViewModel(County selectedCounty, FtpService ftpService, SearchBarViewModel searchBarViewModel)
 		{
 			SelectedCounty = selectedCounty;
-			_ftpService = ftpService;
 			Directories = new ObservableCollection<DirectoryItemViewModel>();
 			SearchBarViewModel = searchBarViewModel;
-			LoadDirectoriesAndFoldersFromFTP();
+			LoadScriptDirectoriesAndFoldersFromFTP(GetRootPath(), ftpService);
 		}
 
-		private void LoadDirectoriesAndFoldersFromFTP()
-		{
-			string rootPath = GetRootPath();
-
-			ObservableCollection<DirectoryItemViewModel> items = _ftpService.LoadDirectoriesAndFilesFromFTP(rootPath);
-
-			foreach (DirectoryItemViewModel item in items)
-			{
-				if (item.Path != null)
-				{
-					string fileExtension = Path.GetExtension(item.Path);
-					if (fileExtension == ".sql" || item.IsDirectory)
-					{
-						Directories.Add(item);
-						item.AddDefaultChildIfEmpty();
-					}
-					else
-					{
-						Log.Error($"Invalid File, {item.Name}, in Admin Apps Timber Exports!");
-					}
-				}
-				else
-				{
-					Log.Error($"Admin Timber Exports, Could not find path for {item.Name}!");
-				}
-			
-			}
-		}
 
 		private string GetRootPath()
 		{
