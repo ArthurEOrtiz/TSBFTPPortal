c2517e58075eb7500d5b95df5e56d40bcbf555d3
Arthur Ortiz <arortiz@tax.idaho.gov>
2023-09-18 10:43:02 -0600

    Report population methods now moved up to ViewModelBase

--- TSBFTPPortal/ViewModels/ViewModelBase.cs
+++ TSBFTPPortal/ViewModels/ViewModelBase.cs
@@ -11,6 +11,11 @@ namespace TSBFTPPortal.ViewModels
 	public class ViewModelBase : INotifyPropertyChanged
 	{
 		private ObservableCollection<DirectoryItemViewModel> _directories;
+		public event PropertyChangedEventHandler? PropertyChanged;
+		protected void OnPropertyChanged(string? propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
 		public ObservableCollection<DirectoryItemViewModel> Directories
 		{
 			get => _directories; 
@@ -20,40 +25,10 @@ namespace TSBFTPPortal.ViewModels
 				OnPropertyChanged(nameof(Directories));
 			}
 		}
+
 		public ViewModelBase()
 		{
 			_directories = new ObservableCollection<DirectoryItemViewModel>();
-		}
-
-		public event PropertyChangedEventHandler? PropertyChanged;
-
-		protected void OnPropertyChanged(string? propertyName)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-		}
-
-		public bool IsScriptFile(string? filePath)
-		{
-			string fileExtension = Path.GetExtension(filePath);
-			string[] scriptExtensions = { ".sql" };
-			return scriptExtensions.Contains(fileExtension, StringComparer.OrdinalIgnoreCase);
-		}
-
-		public void FilterScriptChildItems(DirectoryItemViewModel item)
-		{
-			var copyOfItems = new List<DirectoryItemViewModel>(item.Items);
-
-			foreach (var childItem in copyOfItems)
-			{
-				if (!IsScriptFile(childItem.Name) && childItem.IsFile)
-				{
-					item.Items.Remove(childItem);
-				}
-				else if (childItem.IsDirectory)
-				{
-					FilterScriptChildItems(childItem);
-				}
-			}
 		}
 
 		public void LoadScriptDirectoriesAndFoldersFromFTP(string rootPath, FtpService ftpService)
@@ -65,7 +40,7 @@ namespace TSBFTPPortal.ViewModels
 			{
 				if (item.IsDirectory)
 				{
-					FilterScriptChildItems(item);
+					FilterChildItems(item, IsScriptFile);
 					Directories.Add(item);
 					item.AddDefaultChildIfEmpty();
 				}
@@ -75,6 +50,60 @@ namespace TSBFTPPortal.ViewModels
 				}
 			}
 		}
+
+		public void LoadReportDirectoriesAndFoldersFromFTP(string rootPath, FtpService ftpService)
+		{
+			ObservableCollection<DirectoryItemViewModel> items = ftpService.LoadDirectoriesAndFilesFromFTP(rootPath);
+
+			foreach (DirectoryItemViewModel item in items)
+			{
+				if (item.IsDirectory)
+				{
+					FilterChildItems(item, IsReportFile);
+					Directories.Add(item);
+					item.AddDefaultChildIfEmpty();
+				}
+				else
+				{
+					Directories.Add(item);
+				}
+			}
+		}
+
+		private bool IsScriptFile(string filePath)
+		{
+			string fileExtension = Path.GetExtension(filePath);
+			string[] scriptExtensions = { ".sql" };
+			return scriptExtensions.Contains(fileExtension, StringComparer.OrdinalIgnoreCase);
+		}
+
+		private bool IsReportFile (string filePath)
+		{
+			string fileExtension = Path.GetExtension(filePath);
+			string[] reportExtensions = { ".rpt" };
+			return reportExtensions.Contains(fileExtension, StringComparer.OrdinalIgnoreCase);
+		}
+
+		private void FilterChildItems(DirectoryItemViewModel item, Func<string?, bool> fileCondition)
+		{
+			var copyOfItems = new List<DirectoryItemViewModel>(item.Items);
+
+			foreach (var childItem in copyOfItems)
+			{
+				if (!fileCondition(childItem.Name) && childItem.IsFile)
+				{
+					item.Items.Remove(childItem);
+				}
+				else if (childItem.IsDirectory)
+				{
+					FilterChildItems(childItem, fileCondition);
+				}
+			}
+		}
+
+	
+
+		
 
 
 	}
